8afcb611f7330fb48e2ab69b3659ffdb5b7b3870
Chloe-Woahie <longwelldotpy@gmail.com>
2022-04-22 17:44:35 -0400

    layout

---
+++ examples/draw_circles/src/main.rs
@@ -0,0 +1,58 @@
+use log::error;
+use pixels::{Pixels, SurfaceTexture};
+use std::error::Error;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 800;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Hello Pixels")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    //let mut world = World::new();
+
+    event_loop.run(move |event, _, control_flow| {
+        // Draw the current frame
+        if let Event::RedrawRequested(_) = event {
+            //world.draw(pixels.get_frame());
+            if pixels
+                .render()
+                .map_err(|e| error!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // Handle input events
+        if input.update(&event) {
+            if input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            window.request_redraw();
+        }
+    });
+}
---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+pub fn line(buffer: &mut [u8]) {
+    unimplemented!()
+}
+
+pub fn triangle(buffer: &mut [u8]) {
+    unimplemented!()
+}
+
+pub fn triangle_filled(buffer: &mut [u8]) {
+    unimplemented!()
+}
+
+pub fn circle(buffer: &mut [u8]) {
+    unimplemented!()
+}
+
+pub fn circle_filled(buffer: &mut [u8]) {
+    unimplemented!()
+}
+
+pub fn square(buffer: &mut [u8]) {
+    unimplemented!()
+}
+
+pub fn square_filled(buffer: &mut [u8]) {
+    unimplemented!()
+}
+
+pub fn rect(buffer: &mut [u8]) {
+    unimplemented!()
+}
+
+pub fn rect_filled(buffer: &mut [u8]) {
+    unimplemented!()
+}
